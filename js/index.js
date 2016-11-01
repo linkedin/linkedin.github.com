dfce7b11fa2c542126a58fc9c8a545f6711e9184
Chamath Palihawadana <chamathpali123@gmail.com>
2016-11-01 20:15:39 +0530

    Added stars and forks count initial, need style improvements

--- js/index.js
+++ js/index.js
@@ -140,11 +140,16 @@ var renderPage = function(data) {
     var category = getCategory(item.name);
     isotopeData +=
       '<div class="item ' + category.toLowerCase() + " " + language + ' col-lg-4 border-fade">' +
-      '<h3 class="name">' + item.name + '</h3>' + buildLanguageLabel(language) + '<br><br>' +
+      '<h3 class="name">' + item.name + '</h3>' +
+      '<button type="submit" class="btn_git btn-with-count js-toggler-target"> <svg aria-hidden="true" class="octicon octicon-star" height="16" version="1.1" viewBox="0 0 14 16" width="14"> <path d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path> </svg>'+
+      ' '+ item.stargazers_count +' Stars </button>&nbsp'+
+       '<button type="submit" class="btn_git btn-with-count js-toggler-target"><svg aria-hidden="true" class="octicon octicon-repo-forked" height="16" version="1.1" viewBox="0 0 10 16" width="10"><path d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>'+
+      ' '+ item.forks +' Forks </button>'+
       '<p class="size hidden">' + item.size + '</p>' +
       '<p class="forks hidden">' + item.forks + '</p>' +
       '<p class="watchers hidden">' + item.watchers_count + '</p>' +
       '<p>' + item.description + '</p>' +
+       buildLanguageLabel(language) +
       '</div>';
     var doc;
     if (item.name in repoToDoc) {
