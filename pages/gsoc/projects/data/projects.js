126bd877a4ff7fa3574100d220ac37ca910f63c3
Chamath Palihawadana <chamathpali123@gmail.com>
2017-01-24 16:21:43 +0530

    Init GSoC Projects page

---
+++ pages/gsoc/projects/data/projects.js
@@ -0,0 +1,23 @@
+projects = [
+	{
+		"name" : "Test project",
+		"desc" : "This is a test project.",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"AshanFernando",
+			"rehrumesh",
+			"mjzone"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["Core", "CLI"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	}
+]
