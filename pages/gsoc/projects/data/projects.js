396a971f330deff0ef5347172d59ddab43482627
Chamath Palihawadana <chamathpali123@gmail.com>
2017-01-26 11:36:09 +0530

    Updated projects and admin json data

--- pages/gsoc/projects/data/projects.js
+++ pages/gsoc/projects/data/projects.js
@@ -1,7 +1,7 @@
 projects = [
 	{
-		"name" : "Test project",
-		"desc" : "This is a test project.",
+		"name" : "Protacter Starter: Full fledge dashboard with Firebase",
+		"desc" : "Updating soon",
 		"requirements" : [
 			"The proposal has to contain a clear concept.",
 			"The applicant should have one patch accepted to the coala core."
@@ -11,12 +11,94 @@ projects = [
 			"https://github.com/99xt"
 		],
 		"mentors" : [
-			"AshanFernando",
+			"kasunkv",
 			"rehrumesh",
+			"dilumn"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["Protacter", "Firebase"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "Interns Portal: Adding multi-tenancy",
+		"desc" : "Updating soon",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"ashanF",
 			"mjzone"
 		],
 		"initiatives" : ["GSoC"],
-		"tags" : ["Core", "CLI"],
+		"tags" : ["AngularJS", "Serverless"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "Remote Hardware Policy SDK",
+		"desc" : "Updating soon",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"charithsoori",
+			"dilumn",
+			"sachithp"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["Hardware", "Serverless"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "Live instance for each PR (which can accommodate any web project)",
+		"desc" : "Updating soon",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"ashanF",
+			"rehrumesh"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["github", "PR"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "Extracting User Temporal Profiles from Location Based Social Networks",
+		"desc" : "Updating soon",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"charithsoori",
+			"dilumn",
+			"samudrak"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["data", "social"],
 		"markdown": "improve_diff_handling.md",
 		"collaborating_projects" : ["serverless",]
 	}
