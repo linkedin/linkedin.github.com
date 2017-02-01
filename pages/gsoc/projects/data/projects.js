d9911f7e6ab1c4d8464e0a86c0f1f78cfa7acecc
Chamath Palihawadana <chamathpali123@gmail.com>
2017-02-01 12:26:52 +0530

    Added current projects

--- pages/gsoc/projects/data/projects.js
+++ pages/gsoc/projects/data/projects.js
@@ -101,5 +101,125 @@ projects = [
 		"tags" : ["data", "social"],
 		"markdown": "improve_diff_handling.md",
 		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "Web Application for LogAnalysis Framework",
+		"desc" : "Log Analysis framework provides a platform to extract user interested domain specific information from unstructured log data and present the result in using real-time widgets in a dashboard consisted of graphs, charts, etc.. which help the users to easily comprehend the information. Currently we have built the log analysis framework. Student needs to iplement a complete front-end to visualize the analysis.",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"dileepaj",
+			"mjzone"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["data", "social"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "Automatic Language Detection for LogAnalysis Framework",
+		"desc" : "Updating soon",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"dileepaj",
+			"mjzone"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["data", "social"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "BrainWave analysis library",
+		"desc" : "Updating soon",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"dileepaj",
+			"mjzone"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["data", "social"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "Make Ionic CLI to work with package.json instead of a separate configuration file.",
+		"desc" : "Updating soon",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"kosalanuwan",
+			"mjzone"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["data", "social"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "Socket Connection mediator",
+		"desc" : "Updating soon",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"charithsoori",
+			"dilumn"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["data", "social"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
+	},
+	{
+		"name" : "PaaS framework implementation  with Actor Model",
+		"desc" : "Actor Model is using most of the time for concurrent processing. Rails or any ruby server application can be deployed on top of that PaaS which can be configured to have multiple node actors & a supervisor on top of the nodes. Mailbox of the each actor will be responsible for communication in between actors. The framework should have a mechanism to create, destroy new nodes based on the requirements. And nodes should be able to communicate between each other. Supervisor node should be notified when other nodes go down. A simple Ruby server should be able to deploy on top of the framework",
+		"requirements" : [
+			"The proposal has to contain a clear concept.",
+			"The applicant should have one patch accepted to the coala core."
+		],
+		"difficulty" : "low",
+		"issues" : [
+			"https://github.com/99xt"
+		],
+		"mentors" : [
+			"AshanFernando",
+			"dilumn"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["data", "social"],
+		"markdown": "improve_diff_handling.md",
+		"collaborating_projects" : ["serverless",]
 	}
 ]
