852dbeab7c06b2f9e25e23eaef80f3001ffbe264
Rumesh Hapuarachchi <rehrumesh@hotmail.com>
2017-02-13 23:57:30 +0530

    Updated socket mediator project description

--- pages/gsoc/projects/data/projects.js
+++ pages/gsoc/projects/data/projects.js
@@ -179,7 +179,7 @@ projects = [
 	},
 	{
 		"name" : "Socket Connection mediator",
-		"desc" : "Socket.io and SignalR simplifies the process of ...",
+		"desc" : "Socket and SignalR simplifies the process of ...",
 		"requirements" : [
 			"The proposal has to contain a clear concept."
 		],
