d9371422177b532a37572d24613e0f5a3b406c6d
thinkholic <mailtokasun@gmail.com>
2018-01-23 16:03:52 +0530

    Add new projects

--- pages/gsoc/projects/data/projects.js
+++ pages/gsoc/projects/data/projects.js
@@ -21,209 +21,95 @@ projects = [
 		"collaborating_projects" : ["serverless-react",]
 	},
 	{
-		"name" : "Interns Portal: Adding multi-tenancy",
-		"desc" : "Adding multi tenency to existing application",
-		"requirements" : [
-			"The proposal has to contain a clear concept.",
-			"Understanding about multi tenency",
-			"Auth principals"
-		],
+		"name" : "ASCII fonts to Unicode",
+		"desc" : "Document conversion ASCII fonts to unicode",
+		"requirements" : [],
 		"difficulty" : "medium",
 		"issues" : [
 			"https://github.com/99xt"
 		],
 		"mentors" : [
-			"AshanFernando",
-			"mjzone",
-			"rehrumesh"
+			"thinkholic",
+			"rajikaimal",
+			"kalanaw"
 		],
 		"initiatives" : ["GSoC"],
-		"tags" : ["AngularJS", "Serverless"],
-		"markdown": "interns_portal.md",
-		"collaborating_projects" : ["interns-portal",]
+		"tags" : ["fonts", "ASCII", "Unicode", "JavaScript"],
+		"markdown": "ascii-to-unicode.md",
+		"collaborating_projects" : []
 	},
 	{
-		"name" : "Remote Hardware Policy SDK",
-		"desc" : "Creating a secure policy to access hardware over web.",
-		"requirements" : [
-			"The proposal has to contain a clear concept.",
-			"Solid understanging about Certificate Authorities",
-			"Hardware emulation",
-			"Chrome Extensions"
+		"name" : "Skype integration bot",
+		"desc" : "Skype bot for integrate CI/CI pipelines",
+		"requirements" : [],
+		"difficulty" : "easy",
+		"issues" : [
+			"https://github.com/99xt"
 		],
+		"mentors" : [
+			"rajikaimal",
+			"thinkholic",
+			"lakindu95",
+			"Dilantha111",
+			"buddhics"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["Skype", "Bot", "SkypeBot", "CI", "CD", "API integration", "Bot"],
+		"markdown": "skype-bot.md",
+		"collaborating_projects" : []
+	},
+	{
+		"name" : "wp-artisan",
+		"desc" : "A command-line toolkit for WordPress developers",
+		"requirements" : [],
+		"difficulty" : "medium",
+		"issues" : [
+			"https://github.com/99xt/wp-artisan/issues"
+		],
+		"mentors" : [
+			"thinkholic",
+			"sajeetharan",
+			"kasunkv"
+		],
+		"initiatives" : ["GSoC"],
+		"tags" : ["WordPress", "CMS", "CLI", "NodeJs"],
+		"markdown": "wp-artisan.md",
+		"collaborating_projects" : ["wp-artisan",]
+	},
+	{
+		"name" : "Steroids",
+		"desc" : "A Microservice Framework in TypeScript",
+		"requirements" : [],
 		"difficulty" : "hard",
 		"issues" : [
 			"https://github.com/99xt"
 		],
 		"mentors" : [
-			"charithsoori",
-			"dilumn",
-			"sachithp"
+			"supuncodes",
+			"uslperera",
+			"rifhanakram"
 		],
 		"initiatives" : ["GSoC"],
-		"tags" : ["Hardware", "Certificate Authority", "Hardware"],
-		"markdown": "remote_hw_sdk.md",
-		"collaborating_projects" : ["hardware-policy",]
+		"tags" : ["Serverless", "TypeScript", "JavaScript",  "Nodejs", "Microservices", "Nanoservices", "Unit Testing", "Dependency Injection", "Mocking", "Docker", "Framework", "AWS", "Lambda"],
+		"markdown": "steroids.md",
+		"collaborating_projects" : ["steroids"]
 	},
 	{
-		"name" : "ProjectPR",
-		"desc" : "A platform which allows developers to test each PR with a live instance",
-		"requirements" : [
-			"Application to live test PR of web application."
-		],
+		"name" : "Contentful CMS Media Manager",
+		"desc" : "Media Manager for Contentful Headless CMS / Cross-platform Desktop Application",
+		"requirements" : [],
 		"difficulty" : "medium",
 		"issues" : [
 			"https://github.com/99xt"
 		],
 		"mentors" : [
-			"AshanFernando",
-			"rehrumesh",
-			"asankaf"
+			"Buddhics",
+			"Lakindu95",
+			"dilantha111"
 		],
 		"initiatives" : ["GSoC"],
-		"tags" : ["github", "PR"],
-		"markdown": "live_pr.md",
-		"collaborating_projects" : ["projectpr",]
-	},
-	{
-		"name" : "Extracting User Temporal Profiles from Location Based Social Networks",
-		"desc" : "Location Based Social Networks like Facebook, Twitter, Instagram generates lot of temporal data that explain user’s context.  When you exactly know user temporal profile and if you could find an effective software product that can match business profiles and social user temporal profiles that are in close proximity you can start publishing recommendations for the users who has close proximity behavioral profiles. You need to develop a plugin / lightweight software product that can be easily integrated into existing software product that can provide recommendations based on social user’s temporal data.",
-		"requirements" : [
-			"The proposal has to contain a clear concept."
-		],
-		"difficulty" : "hard",
-		"issues" : [
-			"https://github.com/99xt"
-		],
-		"mentors" : [
-			"charithsoori",
-			"dilumn",
-			"samudrak"
-		],
-		"initiatives" : ["GSoC"],
-		"tags" : ["data", "social", "machine learning"],
-		"markdown": "extracting.md",
-		"collaborating_projects" : ["scikit-api",]
-	},
-	{
-		"name" : "Web Application for LogAnalysis Framework",
-		"desc" : "Log Analysis framework provides a platform to extract user interested domain specific information from unstructured log data and present the result in using real-time widgets in a dashboard consisted of graphs, charts, etc.. which help the users to easily comprehend the information. Currently we have built the log analysis framework. Student needs to iplement a complete front-end to visualize the analysis.",
-		"requirements" : [
-			"The proposal has to contain a clear concept."
-		],
-		"difficulty" : "medium",
-		"issues" : [
-			"https://github.com/99xt"
-		],
-		"mentors" : [
-			"dileepaj",
-			"mjzone",
-			"asankaf"
-		],
-		"initiatives" : ["GSoC"],
-		"tags" : ["data", "social"],
-		"markdown": "log_analysis.md",
-		"collaborating_projects" : ["log_analysis",]
-	},
-	{
-		"name" : "Automatic Language Detection for LogAnalysis Framework",
-		"desc" : "Language detection plugin for log analysis framework.",
-		"requirements" : [
-			"The proposal has to contain a clear concept."
-		],
-		"difficulty" : "hard",
-		"issues" : [
-			"https://github.com/99xt"
-		],
-		"mentors" : [
-			"dileepaj",
-			"mjzone",
-			"asankaf"
-		],
-		"initiatives" : ["GSoC"],
-		"tags" : ["data", "social"],
-		"markdown": "log_analysis_lt.md",
-		"collaborating_projects" : ["log_analysis",]
-	},
-	{
-		"name" : "BrainWave analysis library",
-		"desc" : "This is to come up with an application for Emotiv Insight device (https://www.emotiv.com/insight) device to analyze brain waves and detect how much the brain likes a given taste of ice cream.",
-		"requirements" : [
-			"The proposal has to contain a clear concept."
-		],
-		"difficulty" : "medium",
-		"issues" : [
-			"https://github.com/99xt"
-		],
-		"mentors" : [
-			"dileepaj",
-			"mjzone",
-			"rehrumesh"
-		],
-		"initiatives" : ["GSoC"],
-		"tags" : ["data", "social"],
-		"markdown": "brain.md",
-		"collaborating_projects" : ["brainwave-sdk",]
-	},
-	{
-		"name" : "Make Ionic CLI to work with package.json instead of a separate configuration file.",
-		"desc" : "The mission is to leave the app configuration and app settings from config.xml",
-		"requirements" : [
-			"The proposal has to contain a clear concept."
-		],
-		"difficulty" : "hard",
-		"issues" : [
-			"https://github.com/99xt"
-		],
-		"mentors" : [
-			"kosalanuwan",
-			"mjzone",
-			"charithsoori"
-		],
-		"initiatives" : ["GSoC"],
-		"tags" : ["ionic", "mobile"],
-		"markdown": "ionic.md",
-		"collaborating_projects" : ["ionic"]
-	},
-	{
-		"name" : "Socket Connection mediator",
-		"desc" : "Socket and SignalR simplifies the process of ...",
-		"requirements" : [
-			"The proposal has to contain a clear concept."
-		],
-		"difficulty" : "medium",
-		"issues" : [
-			"https://github.com/99xt"
-		],
-		"mentors" : [
-			"charithsoori",
-			"dilumn",
-			"asankaf"
-		],
-		"initiatives" : ["GSoC"],
-		"tags" : ["data", "social"],
-		"markdown": "socket_mediator.md",
-		"collaborating_projects" : ["socket-mediator",]
-	},
-	{
-		"name" : "PaaS framework implementation  with Actor Model",
-		"desc" : "Actor Model is using most of the time for concurrent processing....",
-		"requirements" : [
-			"The proposal has to contain a clear concept.",
-			"Ruby on Rails knowledge"
-		],
-		"difficulty" : "medium",
-		"issues" : [
-			"https://github.com/99xt"
-		],
-		"mentors" : [
-			"AshanFernando",
-			"dilumn",
-			"rehrumesh"
-		],
-		"initiatives" : ["GSoC"],
-		"tags" : ["data", "social"],
-		"markdown": "paas.md",
-		"collaborating_projects" : ["PIAM",]
+		"tags" : ["Electron" "JavaScript", "HTML", "CSS", "NodeJS", "SQLite"],
+		"markdown": "contentful.md",
+		"collaborating_projects" : []
 	}
 ]
---
+++ pages/gsoc/projects/data/projects/steroids.md
@@ -0,0 +1,31 @@
+## Steroids - A Microservice Framework in TypeScript
+
+### Description
+Steroids Framework enables the developers to write more maintainable, testable, and readable microservices known as Steroids Services using TypeScript. Steroids services can be hosted in environment such as AWS lambda, dockers, or any server instance without any code changes. Therefore this feature enables the developers to choose the best execution environment that meets their performance and functionality goals. This feature also enables the developer to make migration decisions of steroids services (i.e. migrating from AWS Lambda to Docker) even at a later stage of the project.
+
+Steroids allows the developers to write synchronous style code which improves readability and maintainability in their source codes by utilising TypeScript's 'async' and 'await' keywords, and by providing them with necessary components to write Steroids Services.
+
+Steroids not only enables the developers to write synchronous style of microservices, but it also enables the developers to write synchronous style of unit test cases with TypeScript with a syntax similar to C#'s nUnit framework.
+
+Steroids also consists of an IoC (Inversion of Control) container which handles the dependency injection feature of Steroids Services which also enables the developers to write agile code that responds to any requirement change.
+
+Steroids is also extensible which its features can be extended using plug-ins. Plugins can be written to access the database, or storage services (i.e. Amazon S3 or FTP).
+
+
+### Things to do
+- Create command line tool to manage common functionalities in Steroids.
+- Create scaffolders in yeoman to create default Steroids Services and Steroids Microservices.
+- Include missing features to Steroids documentation.
+- Develop new steroids plugins to support various databases and storage mechanisms.
+- Add latest features to the default Steroids boilerplate.
+
+### Requirements
+- Passionate about learning and experimenting with cutting-edge technologies.
+- Ability to come up with  creative, innovative, and unique ideas.
+- Knowledge in TypeScript, JavaScript, and related Design Patterns, AWS Lambda, Docker, Gulp would be helpful.
+
+### Collaborating Projects
+- [Steroids Framework](https://github.com/99xt/steroidslibrary)
+- [Steroids Runtime](https://github.com/99xt/steroidsruntime)
+- [Steroids Boilerplate](https://github.com/99xt/steroidsboilerplate)
+
