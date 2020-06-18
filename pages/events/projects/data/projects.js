aeb0911aec75d7a3e5a8a3540d335f6fd45b9696
Lakindu Gunasekara <LakinduG@99x.lk>
2020-06-18 17:07:02 +0530

    Content

--- pages/events/projects/data/projects.js
+++ pages/events/projects/data/projects.js
@@ -3,7 +3,6 @@ projects = [
     name: "Google Summer of Code - 2020",
     desc: "Google Summer of Code is a global program focused on bringing more student developers into open source software development.",
     target: "University Enrolled Students (Bachlors/Masters)",
-    issues: ["https://summerofcode.withgoogle.com/"],
     dates: "March 16 - September 8",
     eventurl: "https://summerofcode.withgoogle.com/",
     image: "/images/logo-black.png",
