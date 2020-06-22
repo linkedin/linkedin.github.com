bd493252c7d9c5f579c6eb1ae19708ce6bd9ba08
Lakindu Gunasekara <LakinduG@99x.lk>
2020-06-22 09:32:17 +0530

    Adding new events

--- pages/events/projects/data/projects.js
+++ pages/events/projects/data/projects.js
@@ -2,21 +2,51 @@ events = [
   {
     name: "Google Summer of Code - 2020",
     desc: "Google Summer of Code is a global program focused on bringing more student developers into open source software development.",
-    target: "University Enrolled Students (Bachlors/Masters)",
+    target: "Students - University Enrolled Students (Bachlors/Masters), Mentors - Anyone ",
     dates: "March 16 - September 8",
     eventurl: "https://summerofcode.withgoogle.com/",
     image: "/images/events/gsoc.png",
     initiatives: ["events"],
-    tags: ["open-source", "google", "Summer of code"]
+    tags: ["gsoc", "google", "summer-of-code"]
   },
   {
     name: "Hacktoberfest - 2020",
     desc: "Hacktoberfest is a month-long celebration of open source software run by DigitalOcean and DEV. Hacktoberfest is open to everyone in our global community.",
     target: "Anyone",
-    dates: "October",
+    dates: "Will be announced in October 2020",
     eventurl: "https://hacktoberfest.digitalocean.com/",
     image: "/images/events/hacktoberfest.jpg",
     initiatives: ["events"],
-    tags: ["open-source", "digital ocean", "hacktoberfest"]
+    tags: ["open-source", "digital-ocean", "hacktoberfest"]
+  },
+  {
+    name: "OpenStack Summit - 2020",
+    desc: "OpenDev events bring together the developers and users of the open source software powering today's infrastructure, to share best practices, identify gaps, and advance the state of the art in open infrastructure.",
+    target: "Anyone",
+    dates: "June 29 - July 1, 2020, July 20 - 22, 2020 & August 10 - 12, 2020",
+    eventurl: "https://www.openstack.org/events/opendev-2020/",
+    image: "/images/events/opendev.jpg",
+    initiatives: ["events"],
+    tags: ["open-stack", "summit", "opendev"]
+  },
+  {
+    name: "Open Source Summit - 2020",
+    desc: "Open Source Summit + Embedded Linux Conference North America 2020 is now a Virtual Experience, happening June 29 – July 2 on the US Central Time Zone.",
+    target: "Anyone",
+    dates: "June 29 – July 2",
+    eventurl: "https://events.linuxfoundation.org/open-source-summit-north-america/register/",
+    image: "/images/events/oss.png",
+    initiatives: ["events"],
+    tags: ["open-source", "summit", "linux-foundation"]
+  },
+  {
+    name: "Google Season of Docs - 2021",
+    desc: "Season of Docs is an annual program organised by Google. Its goal is to bring technical writers and open source organisations together to foster collaboration and improvement of documentation in the Open Source space.",
+    target: "Anyone",
+    dates: "Will be annouced in 2021",
+    eventurl: "https://developers.google.com/season-of-docs",
+    image: "/images/events/gsod.png",
+    initiatives: ["events"],
+    tags: ["gsod", "google", "season-of-docs"]
   }
 ];
